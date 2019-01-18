298801d7544c223d82a165145990a651dc84b455
Rabie Almatarneh <rabie.almatarneh@focisolutions.com>
2019-01-18 11:18:14 -0500

    Registered new intent

--- src/dialogflow/action-factory.ts
+++ src/dialogflow/action-factory.ts
@@ -14,6 +14,7 @@ import { DefaultWelcome } from './intents/recent-recalls-welcome-intent';
 import { DefaultFallback } from './intents/recent-recalls-default-fallback-intent';
 import { RecentRecallRepeatIntent } from './intents/recent-recalls-repeat.intent';
 import { RecentRecallFallbackIntent } from './intents/recent-recalls-fallback.intent';
+import { RecentRecallAllNo } from './intents/recent recalls-all-no.intent';
 
 export class ActionFactory {
   app: DialogflowApp<
@@ -49,6 +50,8 @@ export class ActionFactory {
     recentRecallsAllPrevious.ApplyIntent();
     const recentRecallsAllNoYes = new RecentRecallAllNoYes(this.app);
     recentRecallsAllNoYes.ApplyIntent();
+    const recentRecallsAllNo = new RecentRecallAllNo(this.app);
+    recentRecallsAllNo.ApplyIntent();
     const recentRecallFallbackIntent = new RecentRecallFallbackIntent(this.app);
     recentRecallFallbackIntent.ApplyIntent();
 
